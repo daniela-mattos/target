bf2a7aa9e876e73fa4566981f19b19a8db5a739a
Daniela Mattos <131220332+daniela-mattos@users.noreply.github.com>
2024-09-24 17:48:34 -0300

    Delete 5 strings/app.js

--- 5 strings/app.js
+++
@@ -1,9 +0,0 @@
-function inverterString(str) {
-  return str.split("").reverse().join("");
-}
-
-const palavra = prompt("Digite uma palavra para inverter:");
-const palavraInvertida = inverterString(palavra);
-
-console.log(palavraInvertida);
-alert(`A palavra invertida é: ${palavraInvertida}`);
